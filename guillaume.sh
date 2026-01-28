15b44f1002034feecdf24b0845269ec193204d3a
Filox <147444295+FIlox77250@users.noreply.github.com>
2026-01-28 10:53:31 +0100

    Refactor guillaume.sh to manage inactive users

--- guillaume.sh
+++ guillaume.sh
@@ -1,21 +1,25 @@
+
 #!/bin/bash
-mkdir -p ./mes_logs
-DATE=$(date +%d-%m-%Y_%Hh%M)
-FICHIER_LOG="./mes_logs/rapport_$DATE.txt"
-UTILISATEUR_FANTOME="ghost_user"
-total=0
-nb_fantomes=0
-if [ -f "/tmp/user_activity.log" ]; then
-
-    while read LIGNE
-    do
-        total=$((total + 1))
-
-        if echo "$LIGNE" | grep -q "$UTILISATEUR_FANTOME"; then
-            nb_fantomes=$((nb_fantomes + 1))
-        fi
-    done < "/tmp/user_activity.log"
-
-    echo "--- Rapport fait le $(date) ---" > "$FICHIER_LOG"
-    echo "Nombre de lignes lues : $total" >> "$FICHIER_LOG"
-    echo "Lignes contenant $UTILISATEUR_FANTOME : $nb_fantomes" >> "$
+JOURS_INACTIFS=30
+FICHIER_INACTIFS="/tmp/utilisateurs_inactifs.txt"
+find /home -maxdepth 1 -type d -atime +$JOURS_INACTIFS | awk -F'/' '{print $3}' > "$FICHIER_INACTIFS"
+if [ -s "$FICHIER_INACTIFS" ]; then
+    echo "Utilisateurs inactifs depuis plus de $JOURS_INACTIFS jours :"
+    cat "$FICHIER_INACTIFS"
+    read -p "Voulez-vous procéder au nettoyage ? [O/N] " REPONSE
+    if [[ "$REPONSE" == "O" || "$REPONSE" == "o" ]]; then
+        while read -r UTILISATEUR; do
+            userdel -r "$UTILISATEUR"
+            if [ $? -eq 0 ]; then
+                echo "Utilisateur $UTILISATEUR supprimé avec succès."
+            else
+                echo "Échec de la suppression de l'utilisateur $UTILISATEUR."
+            fi
+        done < "$FICHIER_INACTIFS"
+    else
+        echo "Nettoyage annulé par l'utilisateur."
+    fi
+else
+    echo "Aucun utilisateur inactif trouvé depuis plus de $JOURS_INACTIFS jours."
+fi
+rm "$FICHIER_INACTIFS"
